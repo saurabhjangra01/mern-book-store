ad9be6b76d7460301a16577d677cb9a9fd409936
Saurabh Jangra <saurabhjangra96@gmail.com>
2024-05-26 23:00:07 +0530

    add escape event listener to BookModal

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { BiUserCircle } from "react-icons/bi";
 
 const BookModal = ({ book, onClose }) => {
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.key === 'Escape') {
+                onClose();
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onClose]);
+
     return (
         <div
             className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
